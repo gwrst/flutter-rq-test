0ef2ebbbd0b839eeaf08009850439c0f03be161b
codeshifters <102579593+codeshifters@users.noreply.github.com>
2022-05-11 09:38:25 +0200

    update

--- challenge_3/lib/main.dart
+++ challenge_3/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: HomeView(
+      home: const HomeView(
         title: 'Recruitment task',
       ),
     );
